a3a958694c99698a2e124771137b9710a78dfea0
aldik1607 <aldiyar.tandau2007@gmail.com>
2025-04-29 21:44:19 +0500

    added BST

---
+++ src/BST.java
@@ -0,0 +1,124 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+    private int size;
+
+    private class Entry {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, value);
+        else if (cmp > 0)
+            node.right = put(node.right, key, value);
+        else
+            node.value = value; // overwrite
+        return node;
+    }
+
+    public V get(K key) {
+        Node current = root;
+        while (current != null) {
+            int cmp = key.compareTo(current.key);
+            if (cmp < 0)
+                current = current.left;
+            else if (cmp > 0)
+                current = current.right;
+            else
+                return current.value;
+        }
+        return null;
+    }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, K key) {
+        if (node == null) return null;
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = delete(node.left, key);
+        else if (cmp > 0)
+            node.right = delete(node.right, key);
+        else {
+            size--;
+            if (node.left == null) return node.right;
+            if (node.right == null) return node.left;
+
+            Node successor = findMin(node.right);
+            node.key = successor.key;
+            node.value = successor.value;
+            node.right = delete(node.right, successor.key);
+        }
+        return node;
+    }
+
+    private Node findMin(Node node) {
+        while (node.left != null)
+            node = node.left;
+        return node;
+    }
+
+    public Iterator<Entry> iterator() {
+        List<Entry> entries = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            entries.add(new Entry(current.key, current.value));
+            current = current.right;
+        }
+
+        return entries.iterator();
+    }
+
+    public int size() {
+        return size;
+    }
+}
